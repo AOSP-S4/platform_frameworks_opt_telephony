LineageOS/android_frameworks_opt_telephony

telephony: CdmaServiceStateTracker: Get cdma sub. when ruim ready.
This code made the assumption that cdma subscription isn't needed
when on cdma + lte. QCOM devices need to get both to provision
propertly because some SIM records are missing.

This patch adds a system property ro.cdma.subscribe_on_ruim_ready
to specify that CDMA subscription should be requested using NV
once the SIM records have been read.

Change-Id: I3e989e9f75c08412bb5f19155beea52e7d9f3c43

src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -95,6 +95,9 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
     private int mNitzUpdateDiff = SystemProperties.getInt("ro.nitz_update_diff",
             NITZ_UPDATE_DIFF_DEFAULT);
 
+    private boolean mSubscribeOnRuimReady = SystemProperties.getBoolean(
+            "ro.cdma.subscribe_on_ruim_ready", false);
+
     private boolean mCdmaRoaming = false;
     private int mRoamingIndicator;
     private boolean mIsInPrl;
@@ -286,7 +289,7 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
             // TODO: Consider calling setCurrentPreferredNetworkType as we do in GsmSST.
             // cm.setCurrentPreferredNetworkType();
 
-            if (phone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
+            if (!mSubscribeOnRuimReady && phone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
                 // Subscription will be read from SIM I/O
                 if (DBG) log("Receive EVENT_RUIM_READY");
                 pollState();

AOSP-S4/platform_frameworks_opt_telephony

Conflicts:
	src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java

src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -92,6 +92,9 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
     private int mNitzUpdateDiff = SystemProperties.getInt("ro.nitz_update_diff",
             NITZ_UPDATE_DIFF_DEFAULT);
 
+    private boolean mSubscribeOnRuimReady = SystemProperties.getBoolean(
+            "ro.cdma.subscribe_on_ruim_ready", false);
+
     private boolean mCdmaRoaming = false;
     private int mRoamingIndicator;
     private boolean mIsInPrl;
@@ -283,7 +286,7 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
             // TODO: Consider calling setCurrentPreferredNetworkType as we do in GsmSST.
             // cm.setCurrentPreferredNetworkType();
 
-            if (mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
+            if (!mSubscribeOnRuimReady && mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
                 // Subscription will be read from SIM I/O
                 if (DBG) log("Receive EVENT_RUIM_READY");
                 pollState();